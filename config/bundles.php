618b08e61094263c852c3d7562f42901cdfd296a
tony dugué <duguetony@gmail.com>
2021-07-18 13:48:59 +0200

    initialisation de Symfony

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
