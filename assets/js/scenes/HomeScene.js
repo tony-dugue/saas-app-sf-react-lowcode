3d3eb6056115324b13c520e099926372512d3a13
tony dugué <duguetony@gmail.com>
2021-07-20 01:14:09 +0200

    ajout de fontawesome

--- assets/js/scenes/HomeScene.js
+++ assets/js/scenes/HomeScene.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from '@fortawesome/free-solid-svg-icons';
 
 const HomeScene = () => {
     return (
-        <h1>Page Home</h1>
+        <h1><FontAwesomeIcon icon={faHome} />Page Home</h1>
     );
 };
 
